6e4a67d902e47763bd8300502caa95e939c87200
TakuHiwa <dawahithird@gmail.com>
2023-09-25 03:21:09 +0900

    Update Arbiter3

--- Arbiter3_230923/Arbiter_Moore.v
+++ Arbiter3_230923/Arbiter_Moore.v
@@ -30,7 +30,21 @@ module Arbiter_Moore(
     output wire Qp0
     );
     
-    assign Qp0 = (Q0 & X2 & ~X1) | (~Q1 & Q0 & X2) | (~X2 & ~X1 & X0) | (~Q0 & X2 & ~X1 & ~X0) | (Q0 & X2 & ~X1);
-    assign Qp1 = (Q1 & ~X2 & X1) | (Q1 & ~Q0 & X1) | (Q1 & Q0 & X2 & X0) | (~X2 & ~X1 & X0) | (~Q0 & ~X2 & X1 & ~X0);
+    wire L01;
+    wire L02;
+    wire L03;
+    assign L01 = X2 ^ X0;
+    assign L02 = (~X1) & L01;
+    assign L03 = L02 + ((X0) & Q1 & (~Q0)) + ((X2) & (~Q1) & (Q0));
+    
+    wire L11;
+    wire L12;
+    wire L13;
+    assign L11 = X0 & (~Q0) + X1 & Q0;
+    assign L12 = X1 & X0 + X2 & L11;
+    assign L13 = Q1 & L12 + ((~X2) & (~X1) & (X0));
+    
+    assign Qp0 = L03;
+    assign Qp1 = L13; 
     
 endmodule
