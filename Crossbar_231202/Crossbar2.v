4e947bc03546fee2ea677ac49620c1d9dcb2bf93
Taku Hiwa <dawahithird@gmail.com>
2023-12-04 12:06:05 +0900

    Crossbarバーエラー修正

--- Crossbar_231202/Crossbar2.v
+++ Crossbar_231202/Crossbar2.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
 module Crossbar2 #(
-    parameter Width = 8, Select = 2
+    parameter Data_Width = 8, Select_Width = 2
     )
     (
     input wire[Data_Width-1:0] X0,
     input wire[Data_Width-1:0] X1,
-    input wire [Select-1:0] S0,
-    input wire [Select-1:0] S1,
+    input wire [Select_Width-1:0] S0,
+    input wire [Select_Width-1:0] S1,
     output wire[Data_Width-1:0] Y0,
     output wire[Data_Width-1:0] Y1
     );
--- Crossbar_231202/MUX2.v
+++ Crossbar_231202/MUX2.v
@@ -1,18 +1,18 @@
 `timescale 1ns / 1ps
 
 module MUX2 #(
-    parameter Width = 8, Select = 2
+    parameter Data_Width = 8, Select_Width = 2
     )
     (
     input wire[Data_Width-1:0] X0,
     input wire[Data_Width-1:0] X1,
-    input wire [Select-1:0] Select
+    input wire [Select_Width-1:0] S,
     output wire[Data_Width-1:0] Y
     );
 
-    wire s0 = (Select == 0)
-    wire s1 = (Select == 1)
+    wire s0 = (S & 'b0);
+    wire s1 = (S & 'b1);
 
-    assign Y = (X0 & s0) | (X1 & s1)
+    assign Y = (X0 & s0) | (X1 & s1);
 
 endmodule
